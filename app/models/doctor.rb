bf771b6d3fd2d6a78d13b3a14c68e79aa5edaa68
nvanderlaan <nathan.vanderlaan@gmail.com>
2015-11-24 00:23:26 -0600

    Add seed file for test database.
    Revise associations to add shortcuts for many to many relationships. Add faker gem for seed file. Fix error in join table migration file.

--- app/models/doctor.rb
+++ app/models/doctor.rb
@@ -1,4 +1,6 @@
 class Doctor < ActiveRecord::Base
   has_many :assignments
+  has_many :offices, through: :assignments
   has_many :specialties
+  has_many :fields, through: :specialties
 end
--- app/models/field.rb
+++ app/models/field.rb
@@ -1,3 +1,4 @@
 class Field < ActiveRecord::Base
   has_many :specialties
+  has_many :doctors, through: :specialties
 end
