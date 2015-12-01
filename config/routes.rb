ddbbefd558ec073b55a9ca07fe15d1a53cf926d8
nvanderlaan <nathan.vanderlaan@gmail.com>
2015-12-01 14:42:54 -0600

    Add doctor profile pages.

--- config/routes.rb
+++ config/routes.rb
@@ -3,5 +3,6 @@ Rails.application.routes.draw do
   root "searches#home"
   get "/:state" => "searches#search"
   get "/:state/results" => "searches#results"
+  get "/:state/offices/:office_id/doctors/:id" => "searches#show"
 
 end
